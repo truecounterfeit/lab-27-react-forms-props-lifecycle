b6dcf6a7852947a086e928a063f54b5004918885
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-12-05 17:41:31 -0800

    update package json and main js

--- src/main.js
+++ src/main.js
@@ -7,23 +7,20 @@ import ReactDom from 'react-dom';
 import superagent from 'superagent';
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
 
     this.state = {
       redditList = [],
     }
-  }
+  };
 
   render() {
     return (
       <div id = "searchform"></div>
     )
-  }
-
-
-
-
+  };
 
 }
 
