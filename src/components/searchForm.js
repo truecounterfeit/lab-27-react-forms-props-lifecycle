f669f4bfe9263ff8a1448600f79b026446ea1017
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2018-01-06 15:41:30 -0800

    refactored searchForm, added error catch

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -12,11 +12,14 @@ class SearchForm extends React.Component {
     this.state = {
       searchText: '',
       posts: [],
-      searchFormLimit: ''
+      postsError: null,
+      searchFormLimit: '',
     };
 
     this.redditValue = this.redditValue.bind(this);
     this.redditResult = this.redditResult.bind(this);
+
+    this.handleSearch = this.handleSearch.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -34,30 +37,48 @@ class SearchForm extends React.Component {
     });
   }
 
-  // handleSearch(e){
-  //   e.preventDefault();
-  //   setState:
-  // }
-  //
-  // handleChange(e){
-  //   e.preventDefault();
-  //   setState: event.target.searchText
-  //   // set target
-  // }
+  handleSearch(e){
+    e.preventDefault();
+    this.setState({
+      posts: req
+    })
+  }
 
   handleSubmit(e){
     e.preventDefault();
     superagent.get(`https://www.reddit.com/r/${this.searchText}.json?limit=${this.searchFormLimit}`)
-    .then((results) => {
-      this.setState({finding: results.body.data.children});
-      // this.setState({board:board})
-    }).catch(console.log('searching...'));
-  }
+    // console.log('searching...');
+
+    .then((res) => {
+      this.setState({
+      posts: res.body.data.children,
+      postsError: null
+      });
+      console.log('found!');
+    })
+
+    .catch(
+      this.setState({
+      posts: null,
+      postsError: name
+      })
+
+    );
+
+    if(!this.state.posts[this.searchText]) {
+      this.setState({
+        posts: null,
+        postsError: name
+      });
+      console.error();
+    };
+
+    }
 
   renderList() {
-    if(this.state.finding)
-      return this.state.finding.map(thread => {
-        return <SearchResultList listItem = {thread} />;
+    if(this.state.posts)
+      return this.state.posts.map(found => {
+        return <SearchResultList listItem = {found} />;
       })
   }
 
@@ -66,11 +87,12 @@ class SearchForm extends React.Component {
       <div className = 'form'>
         <form onSubmit = {this.handleSubmit}>
 
-          <input type = 'text' name = 'search' placeholder = 'Enter search' value = {this.state.redditSubmit} onChange = {this.state.redditValue} />
+          <input type = 'text' name = 'search' placeholder = 'Enter search' value = {this.state.redditSubmit} onChange = {this.state.redditValue} className = 'error'/>
 
-          <input type = 'number' name = 'results' min = '0' max = '10' placeholder = '# of results' value = {this.state.redditThread} onChange = {this.state.redditResult} />
+          <input type = 'number' name = 'results' min = '0' max = '100' placeholder = '# of results' value = {this.state.redditThread} onChange = {this.state.redditResult} />
 
           <button type = 'submit' value = 'submit' onClick={this.handleSubmit}>Search</button>
+
         </form>
         <ul className = 'list' >{this.renderList()}</ul>
       </div>
